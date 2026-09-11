3cf8803d45cb1edf1a5fb3b0c4a29939336ff75b
zhangmin <zhangmin89@outlook.com>
2026-09-12 00:27:10 +0800

    fable 5.1 审核 工程化改造 二阶段

--- tests/CefGlue.Tests/TraceReviewFixTests.cs
+++ tests/CefGlue.Tests/TraceReviewFixTests.cs
@@ -2,6 +2,8 @@ using System;
 using System.IO;
 using System.Reflection;
 using System.Runtime.Loader;
+using System.Security.AccessControl;
+using System.Security.Principal;
 using NUnit.Framework;
 using Xilium.CefGlue.Common.Shared.Serialization;
 
@@ -10,6 +12,69 @@ namespace CefGlue.Tests;
 [TestFixture, NonParallelizable]
 public class TraceReviewFixTests
 {
+    [TestCase(null)]
+    [TestCase("0")]
+    [TestCase("1")]
+    public void DefaultTraceHandlesReadOnlyBaseDirectory(string? value)
+    {
+        const string switchName = "CEFGLUE_TRACE_JAVASCRIPT";
+        const string baseDirectoryKey = "APP_CONTEXT_BASE_DIRECTORY";
+        var previousSwitch = Environment.GetEnvironmentVariable(switchName);
+        var previousBaseDirectory = AppContext.GetData(baseDirectoryKey);
+        var assemblyPath = typeof(Deserializer).Assembly.Location;
+        var directory = Directory.CreateTempSubdirectory("cef-trace-readonly-");
+        var logPath = Path.Combine(directory.FullName, $"javascript-execution-{Environment.ProcessId}.log");
+        var context = new AssemblyLoadContext(directory.Name, isCollectible: true);
+        DirectorySecurity? originalWindowsAccess = null;
+        UnixFileMode? originalUnixMode = null;
+        TextWriter? writer = null;
+        try
+        {
+            if (OperatingSystem.IsWindows())
+            {
+                originalWindowsAccess = directory.GetAccessControl();
+                var readOnlyAccess = directory.GetAccessControl();
+                using var identity = WindowsIdentity.GetCurrent();
+                readOnlyAccess.AddAccessRule(new FileSystemAccessRule(identity.User!, FileSystemRights.Write, AccessControlType.Deny));
+                directory.SetAccessControl(readOnlyAccess);
+            }
+            else
+            {
+                originalUnixMode = File.GetUnixFileMode(directory.FullName);
+                File.SetUnixFileMode(directory.FullName, originalUnixMode.Value & ~(UnixFileMode.UserWrite | UnixFileMode.GroupWrite | UnixFileMode.OtherWrite));
+            }
+
+            // Prove this is an unwritable destination before exercising the real trace initializer.
+            Assert.Throws<UnauthorizedAccessException>(() => File.WriteAllText(logPath, "write probe"));
+            Environment.SetEnvironmentVariable(switchName, value);
+            AppContext.SetData(baseDirectoryKey, directory.FullName);
+            Assert.AreEqual(directory.FullName, AppContext.BaseDirectory);
+            var assembly = context.LoadFromAssemblyPath(assemblyPath);
+            var traceType = assembly.GetType("Xilium.CefGlue.Common.Shared.Helpers.JavascriptExecutionTrace", throwOnError: true)!;
+            Assert.AreEqual(value == "1", traceType.GetProperty("IsEnabled")!.GetValue(null));
+            writer = (TextWriter)traceType.GetField("output", BindingFlags.NonPublic | BindingFlags.Static)!.GetValue(null)!;
+            Assert.AreSame(TextWriter.Null, writer);
+            Assert.DoesNotThrow(() => traceType.GetMethod("Write")!.Invoke(null, new object[] { 42, 7L, "readonly", "test" }));
+            Assert.IsFalse(File.Exists(logPath));
+        }
+        finally
+        {
+            AppContext.SetData(baseDirectoryKey, previousBaseDirectory);
+            Environment.SetEnvironmentVariable(switchName, previousSwitch);
+            writer?.Dispose();
+            context.Unload();
+            if (OperatingSystem.IsWindows())
+            {
+                if (originalWindowsAccess != null) directory.SetAccessControl(originalWindowsAccess);
+            }
+            else if (originalUnixMode.HasValue)
+            {
+                File.SetUnixFileMode(directory.FullName, originalUnixMode.Value);
+            }
+            directory.Delete();
+        }
+    }
+
     [TestCase(false, false)]
     [TestCase(true, false)]
     [TestCase(true, true)]
